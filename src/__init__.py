0c7d8ec4db1a55e828f150262ae163dfe7181d82
Beta Faccion <betafcc@gmail.com>
2018-11-19 14:34:19 -0200

    Adds yaml util

--- src/__init__.py
+++ src/__init__.py
@@ -37,7 +37,7 @@ class ApiDadosScraper(Scraper):
                     "produces",
                     "tags",
                     "paths",
-                    # "definitions",
+                    "definitions",
                 ]
             },
         }
--- src/util.py
+++ src/util.py
@@ -1,3 +1,7 @@
+import oyaml
+from box import Box
+
+
 def camel(snake):
     "convert a snake_case string to camelCase"
     return snake[0].lower() + snake.title().replace("_", "")[1:]
@@ -11,3 +15,23 @@ def commonprefix(m):
         if c != s2[i]:
             return s1[:i]
     return s1
+
+
+class yaml(Box):
+    @classmethod
+    def load(cls, path):
+        with open(path) as fp:
+            return cls(oyaml.load(fp))
+
+    def dump(self, path=None):
+        str = oyaml.dump(self.to_dict(), allow_unicode=True, default_flow_style=False)
+        if not path:
+            return str
+        with open(path, "w") as fp:
+            fp.write(str)
+
+    def __repr__(self):
+        return self.dump()
+
+    def print(self):
+        print(self)
