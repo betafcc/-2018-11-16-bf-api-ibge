1039eec49f250665529dd6349f1a7804fd05ed25
Beta Faccion <betafcc@gmail.com>
2018-11-21 10:34:12 -0200

    Outputs to apis folder

--- src/__main__.py
+++ src/__main__.py
@@ -6,26 +6,21 @@ from src import ServicoDadosScraper
 from src.util import yaml
 
 
-
-
-
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
     logger.addHandler(logging.StreamHandler())
     logger.setLevel(logging.DEBUG)
 
-
-    __dir = Path(__file__).resolve().parent.parent
+    out_dir = Path(__file__).resolve().parent.parent / "apis"
+    out_dir.mkdir(exist_ok=True)
 
     todas = yaml(ServicoDadosScraper().parse())
 
-    file = __dir / 'todas.yaml'
+    file = out_dir / "todas.yaml"
     logger.info(f'writing "todas" to "{file}"')
     todas.dump(file)
 
     for slug in todas:
-        file = __dir / (slug + '.yaml')
+        file = out_dir / (slug + ".yaml")
         logger.info(f'writing "{slug}" to "{file}"')
         todas[slug].dump(file)
-    # path = cwd / 'todas.yaml'
-    # logger.info('escrevendo {}')
