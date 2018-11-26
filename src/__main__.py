b71c65a7ee3607ff8f54df6e25face01e9c70025
Beta Faccion <betafcc@gmail.com>
2018-11-26 03:17:47 -0200

    finishes main

--- src/__main__.py
+++ src/__main__.py
@@ -3,12 +3,12 @@ from pathlib import Path
 import fire
 import requests
 
-from . import api_docs
-from .util import yaml
+from . import ApiDocsHome, parse_api_docs_service
+from .util import yaml, spec_errors, deep_merge
 
 
 def specs_base():
-    return api_docs.asyaml().dump()
+    return ApiDocsHome.from_url().asyaml().dump()
 
 
 def list_specs(specs_base: str):
@@ -26,6 +26,41 @@ def fetch(slug, specs_base):
     url = yaml.load(specs_base)[slug_name].versions[version].externalDocs.url
 
     return requests.get(url).text
+
+
+def parse(slug, html, specs_base, hand_fixes=""):
+    slug_name, version = _split_slug(slug)
+
+    with open(html) as fp:
+        html = fp.read()
+
+    specs_base = yaml.load(specs_base)
+
+    if hand_fixes:
+        try:
+            hand_fixes = yaml.load(hand_fixes)
+        except FileNotFoundError:
+            hand_fixes = yaml({})
+    else:
+        hand_fixes = yaml({})
+
+    result = deep_merge(
+        specs_base[slug_name].versions[version].to_dict(),
+        parse_api_docs_service(
+            html=html,
+            slug_name=slug_name,
+            base_url=specs_base[slug_name].versions[version].externalDocs.url,
+        ).to_dict(),
+        hand_fixes.to_dict(),
+    )
+    return yaml(result).dump()
+
+    # errors = spec_errors(result)
+
+    # if errors:
+    #     raise Exception(spec_errors(result), result)
+    # else:
+    #     return yaml(result).dump()
 
 
 if __name__ == "__main__":
